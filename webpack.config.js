e7a099979d17829b55c0656806c882e74edbafe6
汪東陽 <michael19920327@gmail.com>
2018-12-08 23:42:33 +0800

    ✨ 加回 babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,24 @@ const { resolve } = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: resolve(__dirname, 'dist'),
     library: 'get-some-cool-emojis',
     libraryTarget: 'umd',
     globalObject: "typeof self !== 'undefined' ? self : this",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 };
