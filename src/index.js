abd5972b42965426d1683fcc71b6f19e0c2eb548
汪東陽 <michael19920327@gmail.com>
2018-12-08 19:06:28 +0800

    ✨ 新增函式

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-const a = 'yaya';
+const emojisList = require('emojis-list');
+
+const getSomeCoolEmojis = (num) => {
+  const emojiNumber = parseInt(num);
+  let emojiString = '';
+
+  for (let i = 0; i < emojiNumber; i++) {
+    const randomNumber = Math.floor(Math.random() * (emojisList.length - 1));
+    emojiString += emojisList[randomNumber];
+  }
+
+  return emojiString;
+};
+
+module.exports = getSomeCoolEmojis;
