1154a7d703106016a7ceee2c28b84487484f4938
汪東陽 <michael19920327@gmail.com>
2019-04-03 17:42:50 +0800

    🔧 增加兼容性

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['core-js/features/array/from', './src/index.js'],
   output: {
     filename: 'index.js',
     path: resolve(__dirname, 'dist'),
