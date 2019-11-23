c0c9d7fe817666f801a87126f7eb816e5b4923a1
汪東陽 <michael19920327@gmail.com>
2019-11-24 01:28:51 +0800

    🔧 Update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,20 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true,
-    },
-    extends: 'airbnb-base',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+  },
 };
