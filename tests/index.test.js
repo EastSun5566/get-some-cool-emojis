8e67180672a73f28bfa8643160819e3aed123234
汪東陽 <michael19920327@gmail.com>
2020-01-05 18:17:35 +0800

    Update test

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,8 +2,8 @@ const emojiRegex = require('emoji-regex')();
 
 const getSomeCoolEmoji = require('../src/index');
 
-describe('Test getSomeCoolEmoji function', () => {
-  test('Argument is number', () => {
+describe('getSomeCoolEmoji function', () => {
+  test('arg is number', () => {
     expect(getSomeCoolEmoji(0)).toBe('');
     expect(getSomeCoolEmoji(-3)).toBe('');
     expect(getSomeCoolEmoji(-1.3)).toBe('');
@@ -11,7 +11,7 @@ describe('Test getSomeCoolEmoji function', () => {
     expect(getSomeCoolEmoji(2.3)).toMatch(emojiRegex);
   });
 
-  test('Argument is not number', () => {
+  test('arg is not number', () => {
     expect(getSomeCoolEmoji('')).toBe('');
     expect(getSomeCoolEmoji('test')).toBe('');
     expect(getSomeCoolEmoji(null)).toBe('');
