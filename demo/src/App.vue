925a55aa8383333ffd1dff2ab2b92c39812f77d0
汪東陽 <michael19920327@gmail.com>
2021-03-20 16:45:47 +0800

    chore(demo): update deps

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -31,7 +31,6 @@
 
 <script lang="ts">
 import { defineComponent, ref, computed } from 'vue';
-// @ts-ignore
 import getSomeCoolEmojis from 'get-some-cool-emojis';
 
 import { GithubCorner, Note } from './components';
